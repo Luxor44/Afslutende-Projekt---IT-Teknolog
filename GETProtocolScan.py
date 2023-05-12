9b83fb3739edf1d38f90702d462272246ef094fd
Luxor44 <92293868+Luxor44@users.noreply.github.com>
2023-05-12 19:09:37 +0200

    Create GETProtocolScan.py

---
+++ GETProtocolScan.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import os
+import requests
+BearerToken = os.getenv('BearerToken')
+url = "http://192.168.8.174:5000/api/get/portscan"
+
+
+def get_api_data():
+    headers = {"Authorization": "Bearer "+BearerToken, "Content-Type": "application/json"}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print('Error: Unable to retrieve data from API')
+        return None
+
+# assign each item in the data recieved
+data = get_api_data()
+if data:
+    for item in data:
+        ip_address = item['IpAdress']
+        mac_address = item['MacAdress']
+        ports = item['Ports']
+        id = item['id']
+        print(f'Item {id}: IP Address={ip_address}, MAC Address={mac_address}, Ports={ports}')
